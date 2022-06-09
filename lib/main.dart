48255e2efc3e2cbcc54fd64fb451157793fa4f31
Sanjay Makwana <63788069+sunnymac@users.noreply.github.com>
2022-06-09 11:13:32 +0530

    task complete

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,10 @@ class MyApp extends StatelessWidget {
                   height: 150,
                   width: 100,
                   //color: Colors.orange,
-                  child: Image.asset("assets/images/person.jpg", 
+                  child: ClipRRect(
+                     borderRadius: BorderRadius.circular(16.0),
+                    child: Image.asset("assets/images/person.jpg", 
+                    ),
                   ),
                  
                 ),
@@ -61,38 +64,56 @@ class MyApp extends StatelessWidget {
                       ),
                       Row(
                         children: [
-                          Container(
-                            height: 30,
-                            width: 40,
-                            decoration: BoxDecoration(
-                                color: Color.fromRGBO(253, 236, 221, 1),
-                              borderRadius: BorderRadius.all(Radius.circular(10))
-                            ),
-                          
-                            child: Icon(Icons.mail, color: Color.fromRGBO(250, 186, 123, 1),)),
+                          InkWell(
+                            onTap: (){
+print("Clicked on Mail");
+
+                                  },
+                            child: Container(
+                              height: 30,
+                              width: 40,
+                              decoration: BoxDecoration(
+                                  color: Color.fromRGBO(253, 236, 221, 1),
+                                borderRadius: BorderRadius.all(Radius.circular(10))
+                              ),
+                            
+                              child: Icon(Icons.mail, color: Color.fromRGBO(250, 186, 123, 1),)),
+                          ),
                             SizedBox(
                               width: 10,
                             ),
-                                Container(
-                            height: 30,
-                            width: 40,
-                            decoration: BoxDecoration(
-                                color: Color.fromRGBO(255, 242, 240, 1),
-                              borderRadius: BorderRadius.all(Radius.circular(10))
-                            ),
-                          
-                            child: Icon(Icons.call, color: Color.fromRGBO(242, 146, 151, 1),)),
+                                InkWell(
+                                  onTap: (){
+print("Clicked on Call");
+
+                                  },
+                                  child: Container(
+                                                            height: 30,
+                                                            width: 40,
+                                                            decoration: BoxDecoration(
+                                  color: Color.fromRGBO(255, 242, 240, 1),
+                                                              borderRadius: BorderRadius.all(Radius.circular(10))
+                                                            ),
+                                                          
+                                                            child: Icon(Icons.call, color: Color.fromRGBO(242, 146, 151, 1),)),
+                                ),
                             SizedBox(width: 10,),
                           
-                              Container(
-                            height: 30,
-                            width: 40,
-                            decoration: BoxDecoration(
-                                color: Color.fromRGBO(235, 237, 238, 1),
-                              borderRadius: BorderRadius.all(Radius.circular(10))
-                            ),
-                          
-                            child: Icon(Icons.video_call_outlined, color: Color.fromRGBO(168, 170, 185, 1),)),
+                              InkWell(
+                                onTap: (){
+print("Clicked on Video Call");
+
+                                  },
+                                child: Container(
+                                                          height: 30,
+                                                          width: 40,
+                                                          decoration: BoxDecoration(
+                                  color: Color.fromRGBO(235, 237, 238, 1),
+                                borderRadius: BorderRadius.all(Radius.circular(10))
+                                                          ),
+                                                        
+                                                          child: Icon(Icons.video_call_outlined, color: Color.fromRGBO(168, 170, 185, 1),)),
+                              ),
                           
 
                           
@@ -180,7 +201,9 @@ Row(
     ),
     Container(
 
-      child: Image.asset("assets/images/map.png", width: 120),
+      child: ClipRRect(
+          borderRadius: BorderRadius.circular(16.0),
+        child: Image.asset("assets/images/map.png", width: 120)),
     )
   ],
 ),
@@ -200,29 +223,58 @@ Row(
          borderRadius: BorderRadius.circular(20)
       ),
       alignment: Alignment.center,
-      height: 100,
+      height: 80,
       width: 150,
      
-      child: Text("Follow me on Youtube", style: TextStyle(
-        fontSize: 18,
-        color: Colors.white,
-        fontWeight: FontWeight.bold
-      ),),
+      child: Row(
+        children: [
+          SizedBox(width: 10,),
+          Icon(Icons.schedule, color: Colors.white),
+          SizedBox(width: 10,),
+          SizedBox(
+            width: 80,
+            child: Text("List of Schedule", 
+             overflow: TextOverflow.ellipsis,
+              maxLines: 50,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 18
+            ),),
+          )
+        ],
+      )
     ),
-    Container(
-       alignment: Alignment.center,
-      height: 100,
-      width: 150,
-       decoration: BoxDecoration(
+Container(
+      decoration: BoxDecoration(
          color: Color.fromRGBO(165, 165, 165, 1),
          borderRadius: BorderRadius.circular(20)
       ),
-      child: Text("Follow me on Instagram", style: TextStyle(
-        fontSize: 18,
-        color: Colors.white,
-        fontWeight: FontWeight.bold
-      ),),
+      alignment: Alignment.center,
+      height: 80,
+      width: 150,
+     
+      child: Row(
+        children: [
+          SizedBox(width: 10,),
+          Icon(Icons.schedule, color: Colors.white),
+          SizedBox(width: 10,),
+          SizedBox(
+            width: 80,
+            child: Text("List of Schedule", 
+             overflow: TextOverflow.ellipsis,
+              maxLines: 50,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 18
+            ),),
+          )
+        ],
+      )
     ),
+
+   
       
   ],
 )
