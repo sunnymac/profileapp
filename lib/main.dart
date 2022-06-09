b2f840dd17a9dea753016ed4fb21b913d36db5e6
Sanjay Makwana <63788069+sunnymac@users.noreply.github.com>
2022-06-09 09:38:27 +0530

    basic code done

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,85 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
+//Color(0xFFB74093)
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
      
-      home: Scaffold()
+      home: Scaffold(
+        backgroundColor: Colors.grey ,
+        appBar: AppBar(
+          title: Text("My Profile"),
+          backgroundColor: Colors.blueGrey,
+          centerTitle: true,
+        ),
+        body: Column(children: [
+          Row(
+            children: [
+              Container(
+                height: 100,
+                width: 50,
+                color: Colors.orange,
+              ),
+              Column(
+                children: [
+                  Text("Name"),
+                  Text("Desgi"),
+                  Row(
+                    children: [
+                      Icon(Icons.access_alarm),
+                      Icon(Icons.access_alarm),
+                      Icon(Icons.access_alarm)
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
+
+Text("About"),
+Text("Paragraph"),
+Container(
+  child: Row(
+    children: [
+      Column(
+        children: [
+          Row(
+            children: [
+              Icon(Icons.access_alarm),
+              Text("Address")
+            ],
+          ),
+          Row(
+            children: [
+              Icon(Icons.access_alarm),
+              Text("Address")
+            ],
+          )
+        ],
+      ),
+      Container(
+        child: Text("Map"),
+      )
+    ],
+  ),
+),
+Text("Activity"),
+Row(
+  children: [
+    Container(
+      child: Text("left"),
+    ),
+      Container(
+      child: Text("right"),
+    )
+  ],
+)
+
+        ]),
+      )
     );
   }
 }
